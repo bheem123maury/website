df696b063f4d24d1120d2183ef7c12fb4d54f7cf
Bheem maurya <105773809+bheem123maury@users.noreply.github.com>
2022-12-21 09:28:23 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,8 @@
+function first(){
+    document.getElementById("slideimage").src="t1.png";
+}
+setInterval (first,2000);
+function second(){
+    document.getElementById("slideimage").src="op.jpg";
+}
+setInterval (second,4000);
